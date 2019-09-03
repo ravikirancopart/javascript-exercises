575a91f74d5b4da36a02082598920a4859c0aa25
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-03 15:19:01 +0530

    removing specified elements from Array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+//arrayEle=["hey", 2, 3, "ho"]   ...removeEle=   "hey", 3
+//first we will filter arrayEle
+//we will compare each ele(arrayEle) with removeEle 
+// we will return the ele when it is not present in removeEle
 
-}
 
+const removeFromArray = function(arrayEle,...removeEle) {
+    return arrayEle.filter(ele=>{
+      return !removeEle.includes(ele);
+    });
+ }
+var result=removeFromArray(["hey", 2, 3, "ho"], "hey", 3);            // output: [2,"ho"]
+console.log("result",result);
 module.exports = removeFromArray
+
+
+
+
+
+
+
