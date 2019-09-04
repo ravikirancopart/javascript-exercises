21ef63b64be6a4a33aeab9ca8f6eee2bfd745afe
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-04 16:28:58 +0530

    palindrome of a string

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    str = str.toLowerCase().replace(/\W/g, "");
+    var len = str.length - 1
+    for (var i = 0; i < len / 2; i++) {
+      if (str[i] !== str[len - i]) {
+        return false;
+      }
+    }
+    return true;
 
 }
-
+console.log(palindromes('Animal loots foliated detail of stool lamina.'))
 module.exports = palindromes
+
+
+
+// function palindrome(str) {
+//     str = str.toLowerCase().replace(/\W/g, "");
+//     for (var i = 0, len = str.length - 1; i < len / 2; i++) {
+//       if (str[i] !== str[len - i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+//    console.log(palindrome('racecar'));
+  
