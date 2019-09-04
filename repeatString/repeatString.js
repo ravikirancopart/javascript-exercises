f296d81511a8814e74049edd3db4db71a8e76c31
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-04 12:06:00 +0530

    repeatString specified num of times

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
 
+const repeatString = function(arg,num) {
+    var res='';
+    if(num==0)
+    {
+        return res
+    }
+    else if(num<=0)
+    {
+        return "ERROR";
+    }
+    else 
+    {
+     for(var i=1;i<=num;i++)
+        {
+             res=res.concat(arg);
+        } 
+    return res;
+    }
 }
-
+console.log(repeatString('hey',1))
+console.log('--------')
+console.log(repeatString('hey', 3))
 module.exports = repeatString
