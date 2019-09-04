50e6a069f60e00e6e75252c9de35914afaa703a7
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-04 17:34:30 +0530

    Temp convert CtF viceversa

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var rounded=Math.round( fToCel*10)/10
+  var message =  rounded;
+    return message;
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var rounded=Math.round(  cToFahr*10)/10
+  var message = rounded;
+   return message;
 }
-
+console.log(ftoc(100));
+console.log(ctof(73.2));
 module.exports = {
   ftoc,
   ctof
 }
+
+
