6004f543a39176118a43bbf594325279116a7023
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-04 15:57:14 +0530

    reverse a given string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+var res;
+const reverseString= (str) =>{
+    var newStr = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
 }
-
+console.log(reverseString("123! abc!"))
 module.exports = reverseString
+
+
+
