83926b3d6b078426e8b4b0c05d50e0492ac83ba5
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-04 10:18:53 +0530

    Leap Year

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+var f1=1;
+var f2=1;
+var f3;
+const fibonacci =function(Arg) {
+    for(var i=2;i<=Arg;i++)
+    {
+      f3=f1+f2;
+      f1=f2;
+      f2=f3;
+      
+    }
+     console.log(f1) 
 }
-
+fibonacci(4);
 module.exports = fibonacci
+
+ 
--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    return (year%100==0)?((year%400==0)?true:false):((year%4==0)?true:false);
 }
-
+console.log(leapYears(400));
 module.exports = leapYears
+
