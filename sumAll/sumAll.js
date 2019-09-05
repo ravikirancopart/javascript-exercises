8febaf15d4a827271308b10efb54317e153ddbcc
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-05 11:13:53 +0530

    sum of numbers in given range

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(start,End) {
+        var sum=0;
+        if((start<0||End<0)||Array.isArray(start) ||Array.isArray(End)||(start.constructor===String)||(End.constructor===String))
+        {
+            return "ERROR";
+        }
+        else if(End>0 && start>End)
+        {
+            for(var i=start;i>=End;i--)
+            {
+            sum=sum+i;
+            }
+        return sum;
+        }
+        else(start>0 && start<End  )
+        {
+            for(var j=start;j<=End;j++)
+            {
+                sum=sum+j;
+            }
+            return sum;
+        }
+        
 }
+console.log(sumAll(1,[]));
 
 module.exports = sumAll
