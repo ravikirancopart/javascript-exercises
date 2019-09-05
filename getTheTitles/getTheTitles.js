e0878e76daafbed2bd6a53009c7209d5d26ed5a7
Sharanya Nagapuri <Sharanya.Nagapuri@copart.com>
2019-09-05 10:07:34 +0530

    get the title from Array of objects

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,23 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
+const getTheTitles = function(arg) {
+    var result=[];
+    arg.map(para=>
+    {
+      return result.push(para.title);
+    });
+      return result;
 }
-
+console.log(getTheTitles(books));       //['Book','Book2']
 module.exports = getTheTitles;
+
+
+
